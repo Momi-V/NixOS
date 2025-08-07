f579c244197dfae859123f9f2ca972c666a69764
HPPinata <83947761+HPPinata@users.noreply.github.com>
2025-08-07 23:43:20 +0200

    enable jack

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -110,6 +110,8 @@ in
     pulse.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
+    # Uncomment the following line if you want to use JACK applications
+    jack.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).

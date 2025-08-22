e6e1590805857a14561ccb5d4f5c6da5e7527f12
HPPinata <83947761+HPPinata@users.noreply.github.com>
2025-08-22 23:57:33 +0200

    steam compatibility

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -133,6 +133,8 @@ in
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    gamescopeSession.enable = true; # Use the gamescope compositor, enables resolution upscaling and stretched aspect ratios
+    extraCompatPackages = [ pkgs.proton-ge-bin ]; # Install Proton-GE
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.

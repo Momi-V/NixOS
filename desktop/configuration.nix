559d74b1bed0bced34e4d5784fb3b394c3f2de88
HPPinata <83947761+Momi-V@users.noreply.github.com>
2025-10-01 09:59:52 +0200

    kwin sddm

--- desktop/configuration.nix
+++ desktop/configuration.nix
@@ -80,6 +80,7 @@ in
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.autoNumlock = true;
   services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.wayland.compositor = "kwin";
   services.desktopManager.plasma6.enable = true;
   programs.dconf.enable = true;
 

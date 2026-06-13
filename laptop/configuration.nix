bef1f195fd5b89274943620872b0156cfe7a53d0
Momi-V <83947761+Momi-V@users.noreply.github.com>
2026-06-13 16:12:31 +0200

    drop auto-cpufreq

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -53,18 +53,6 @@ in
     enable = true;
   };
 
-  # services.auto-cpufreq.enable = true;
-  # services.auto-cpufreq.settings = {
-  #   battery = {
-  #     governor = "powersave";
-  #     turbo = "never";
-  #    };
-  #    charger = {
-  #      governor = "performance";
-  #      turbo = "auto";
-  #    };
-  # };
-
   # Networking
   networking.hostName = "AshFlake"; # Define your hostname.
   # Pick only one of the below networking options.

0b0d816cca6d18ba31624d8560e5f611c1e1cd84
Momi-V <83947761+Momi-V@users.noreply.github.com>
2026-06-13 07:26:17 +0200

    x64_v4 Fixes

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -197,6 +197,41 @@ in
     ];
   };
 
+  # x64_v4 Fixes
+  nixpkgs.overlays = [
+    (final: prev: {
+      # https://github.com/assimp/assimp/issues/6342
+      assimp = prev.assimp.overrideAttrs (old: {
+        NIX_CFLAGS_COMPILE =
+          (old.NIX_CFLAGS_COMPILE or "") + " -ffp-contract=on";
+      });
+
+      # https://github.com/godotengine/godot/issues/91217
+      # https://github.com/godotengine/godot/pull/95158
+      embree = prev.embree.overrideAttrs (old: {
+        cmakeFlags = (old.cmakeFlags or []) ++ [
+          "-DEMBREE_ISA_SSE2=OFF"
+          "-DEMBREE_ISA_SSE42=OFF"
+        ];
+      });
+
+      # https://lists.xenproject.org/archives/html/xen-devel/2025-01/msg00439.html
+      xen = prev.xen.overrideAttrs (old: {
+        patches = (old.patches or []) ++ [
+          (prev.writeText "xen-text-alignment.patch" ''
+            --- a/xen/arch/x86/boot/Makefile
+            +++ b/xen/arch/x86/boot/Makefile
+            @@ -44,2 +44,2 @@
+            -text_gap := 0x010200
+            -text_diff := 0x408020
+            +text_gap := 0x010240
+            +text_diff := 0x608040
+          '')
+        ];
+      });
+    })
+  ];
+
   # Virtualization
   # virtualisation.libvirtd.enable = true;
   # virtualisation.libvirtd.qemu.swtpm.enable = true;

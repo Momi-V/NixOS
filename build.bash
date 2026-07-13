0b3aba368da396c0e5a4f99e3fa79a92772c3854
Momi-V <83947761+Momi-V@users.noreply.github.com>
2026-07-13 03:34:14 +0200

    abort on fail

--- build.bash
+++ build.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo nixos-rebuild build --upgrade-all --keep-going -I nixos-config=./desktop/configuration.nix
+sudo nixos-rebuild build --upgrade-all -I nixos-config=./desktop/configuration.nix

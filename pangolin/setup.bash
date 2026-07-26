8c52d20f9f479bb08c6e10c24d190b99d81a3b17
Momi-V <83947761+Momi-V@users.noreply.github.com>
2026-07-26 03:45:37 +0200

    no envfs during install

--- pangolin/setup.bash
+++ pangolin/setup.bash
@@ -32,12 +32,19 @@ nixos-generate-config --root /mnt
 curl -o /mnt/etc/nixos/configuration.nix https://raw.githubusercontent.com/Momi-V/NixOS/refs/heads/main/pangolin/configuration.nix
 sleep 1
 
+echo "Patching Config (no envfs during install)"
+cp /mnt/etc/nixos/configuration.nix /mnt/etc/nixos/configuration.nix.envfs
+sed -i 's+services.envfs.enable+#services.envfs.enable+g' /mnt/etc/nixos/configuration.nix
+sed -i 's+programs.nix-ld.enable+#programs.nix-ld.enable+g' /mnt/etc/nixos/configuration.nix
+sleep 1
+
 echo "Installing NixOS"
 cd /mnt
 nix-channel --update
 nixos-install
 sleep 1
 
-echo "Copying root SSH keys"
+echo "Copying root SSH keys and restoring config"
 cp -r /root/.ssh/ /mnt/root/
+mv /mnt/etc/nixos/configuration.nix.envfs /mnt/etc/nixos/configuration.nix
 sleep 1

22efa22f5a1349efabab1e32683aa55a978fcf15
Momi-V <83947761+Momi-V@users.noreply.github.com>
2026-08-10 15:30:13 +0200

    easyeffects

--- laptop/configuration.nix
+++ laptop/configuration.nix
@@ -165,7 +165,7 @@ in
       amdgpu_top btop fastfetch screen mission-center
       btrfs-assistant kdePackages.filelight
       kdePackages.kcalc kdePackages.kcharselect meld
-      virt-manager docker-compose
+      virt-manager docker-compose easyeffects
       cemu winetricks mangohud
     ];
   };
